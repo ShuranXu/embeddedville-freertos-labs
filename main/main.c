a870e20e36e3eb8390891f060c946274374baf14
Shuran Xu <shuran.xu@mail.utoronto.ca>
2026-09-19 16:46:16 -0400

    Flush starter boot evidence for QEMU

--- main/main.c
+++ main/main.c
@@ -9,6 +9,7 @@
 void app_main(void)
 {
     printf("EVIDENCE boot idf=%s\n", IDF_VER);
+    fflush(stdout);
     /* TODO: build the supervised producer/consumer sensor-hub task graph. */
     /* TODO: configure an explicit power policy and track useful progress. */
 }
